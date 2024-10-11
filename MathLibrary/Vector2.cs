7c2d883e4a331fc89bf4562268eeaed7fe253642
sfaulk7 <sfaulk.busy@gmail.com>
2024-10-11 10:25:40 -0500

    Made Vector2

--- MathLibrary/Vector2.cs
+++ MathLibrary/Vector2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace MathLibrary
 {
@@ -41,9 +42,27 @@ namespace MathLibrary
         }
 
         //Operator overload for multipllication by a scaler
+        public static Vector2 operator *(Vector2 left, float right)
+        {
+            return new Vector2(left.x * right, left.y * right);
+        }
+
+        //Operator overload for division
         public static Vector2 operator /(Vector2 left, Vector2 right)
         {
             return new Vector2(left.x / right.x, left.y / right.y);
+        }
+
+        //implicit conversion from System.Numerics.Vector2 to Vector2
+        public static implicit operator Vector2(System.Numerics.Vector2 vector)
+        {
+            return new Vector2(vector.X, vector.Y);
+        }
+
+        //implicit conversion from Vector2 to System.Numerics.Vector2
+        public static implicit operator System.Numerics.Vector2(Vector2 vector)
+        {
+            return new System.Numerics.Vector2(vector.x, vector.y);
         }
     }
 }
